41bf5844ec202c9ab39a66d2d0e5732f5a8f14b8
Dgonza19 <Daisy.gu07@gmail.com>
2023-11-08 14:20:00 -0500

    added code to script files

--- scripts/index.js
+++ scripts/index.js
@@ -3,27 +3,46 @@
 window.onload = init;
 
 function init(){
-const showMeTheJokesBtn = document.getElementById("showMeTheJokesBtn");
-showMeTheJokesBtn.onclick = showMeTheJokesBtnClicked;
+    const showMeTheJokesBtn = document.getElementById("showMeTheJokesBtn");
+    showMeTheJokesBtn.onclick = showMeTheJokesBtnClicked;
 
-populateCategoriesDropdown();
-
-
+    populateCategoriesDropdown();
 
 
-};
+}
 
 
 function populateCategoriesDropdown(){
-// populate the categories dropdown with distinct categories from the jokes array.
-  
+    //populate categories dropdown with distinct categories from the jokes array;
+    let categories = getCategoriesArray();
 
+    for(let category of categories){
+        const categoriesSelect = document.getElementById("categoriesSelect");
+        let newOption = new Option(category, category);
+        categoriesSelect.appendChild(newOption);
+    }
 
-};
+}
 
+function getCategoriesArray(){
+    let categories = [];
+    for( let dadJoke of jokes.dadJokes){
+        //console.log(dadJoke);
+        if(categories.includes(dadJoke.category) != true){
+            categories.push(dadJoke.category);
+        }
+    }
+
+    //now I have my categories, lets sort them...
+
+    categories.sort();
+console.log(categories);
+    return categories;
+}
 
 
 function showMeTheJokesBtnClicked(){
-  // this will id the selected category. and loop througha nd show those jokes
+    //identify the selected category, and loop through and show those jokes!
 
-};
+
+}
