644d9d69a8fab96fec61eaf7aa2faa56366f0b6c
Dgonza19 <Daisy.gu07@gmail.com>
2023-11-08 13:47:58 -0500

    gave jokes a var. started to set up project

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+window.onload = init;
+
+function init(){
+const showMeTheJokesBtn = document.getElementById("showMeTheJokesBtn");
+showMeTheJokesBtn.onclick = showMeTheJokesBtnClicked;
+
+populateCategoriesDropdown();
+
+
+
+
+};
+
+
+function populateCategoriesDropdown(){
+// populate the categories dropdown with distinct categories from the jokes array.
+  
+
+
+};
+
+
+
+function showMeTheJokesBtnClicked(){
+  // this will id the selected category. and loop througha nd show those jokes
+
+};
